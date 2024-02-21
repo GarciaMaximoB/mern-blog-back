86d80fb3b74c9330b4e306c20c0c2494f96b31fe
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-02-21 20:12:26 -0300

    Agregamos el back

---
+++ index.js
@@ -0,0 +1,127 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const cors = require("cors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({
+  dest: "uploads/",
+  limits: {
+    fileSize: 300 * 1024 * 1024, // 30 MB (por ejemplo)
+  },
+});
+
+const fs = require("fs");
+const Post = require("./models/Post");
+const app = express();
+
+const salt = bcrypt.genSaltSync(10);
+const secret = "asd123";
+
+app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use(cookieParser());
+app.use("/uploads", express.static(__dirname + "/uploads"));
+
+mongoose.connect(
+  "mongodb+srv://maximogarcia:9kdndZsyXVefBBuG@cluster0.ngbwiva.mongodb.net/?retryWrites=true&w=majority"
+);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+
+  if (passOk) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json({
+        id: userDoc._info,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json("Credenciales incorrectas");
+  }
+});
+
+app.get("/profile", (req, res) => {
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
+});
+
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
+  let newPath = null;
+  if (req.file) {
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    newPath = path + "." + ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const { token } = req.cookies;
+  jwt.verify(token, secret, {}, async (err, info) => {
+    if (err) throw err;
+    const { id, title, summary, content } = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json("you are not the author");
+    }
+
+    await Post.findOneAndUpdate(
+      { _id: id }, // Filter
+      {
+        $set: {
+          title,
+          summary,
+          content,
+          cover: newPath ? newPath : postDoc.cover,
+        },
+      }, // Update
+      { new: true } // Options to return the updated document
+    );
+
+    res.json(postDoc);
+  });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
+});
+
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params;
+  const postDoc = await Post.findById(id).populate("author", ["username"]);
+  res.json(postDoc);
+});
+
+app.listen(4000);
